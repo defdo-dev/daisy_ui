e12a5f859eac4a9e4fad5579c89a32119137e473
Roberto Estrada <restrada@paridin.com>
2022-07-21 23:53:02 -0500

    blank project to keep a clean project

--- test/daisy_ui_web/controllers/page_controller_test.exs
+++ test/daisy_ui_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule DaisyUiWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "defdo"
   end
 end
