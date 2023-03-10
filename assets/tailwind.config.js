43fbc9872cbaf2a52cbf0911d0a2e1f453305ab1
Roberto Estrada <restrada@paridin.com>
2023-03-10 14:20:32 -0600

    update and use hero icon plugin

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -2,47 +2,40 @@
 // See the Tailwind configuration guide for advanced usage
 // https://tailwindcss.com/docs/configuration
 
-let plugin = require('tailwindcss/plugin')
+const plugin = require("tailwindcss/plugin")
+const fs = require("fs")
+const path = require("path")
+
+let baseColor = {
+  "primary": "#F9BC02",
+  "secondary": "#330054",
+  "accent": "#ff91af",
+  "neutral": "#f4efea",
+  "base-100": "#140021",
+  "info": "#9ad4f4",
+  "success": "#a3e635",
+  "warning": "#fde047",
+  "error": "#f32c3f",
+}
 
 module.exports = {
   content: [
-    './js/**/*.js',
-    '../lib/*_web.ex',
-    '../lib/*_web/**/*.*ex'
+    "./js/**/*.js",
+    "../lib/*_web.ex",
+    "../lib/*_web/**/*.*ex"
   ],
   daisyui: {
     themes: [
       {
-        defdo_light: {
-          "primary": "#F9BC02",
-          "secondary": "#ff962b",
-          "accent": "#ff91af",
-          "neutral": "#1e1d35",
-          "base-100": "#FCFCFC",
-          "info": "#9ad4f4",
-          "success": "#a3e635",
-          "warning": "#fde047",
-          "error": "#f32c3f",
-        },
-        defdo_dark: {
-          "primary": "#F9BC02",
-          "secondary": "#ff962b",
-          "accent": "#ff91af",
-          "neutral": "#1e1d35",
-          "base-100": "#32323C",
-          "info": "#9ad4f4",
-          "success": "#a3e635",
-          "warning": "#fde047",
-          "error": "#f32c3f",
-        },
+        defdo_dark: baseColor,
+        defdo_light: { ...baseColor, "neutral": "#140021", "base-100": "#f4efea" },
       },
     ],
   },
   theme: {
     extend: {
       fontFamily: {
-        headline: ["Saira Condensed", "System-ui"],
-        mono: ["JetBrains Mono"]
+        code: ["JetBrains Mono"]
       },
       screens: {
         xs: '340px',
@@ -50,11 +43,50 @@ module.exports = {
     },
   },
   plugins: [
-    require("@tailwindcss/typography"),
-    require("daisyui"),
-    plugin(({ addVariant }) => addVariant('phx-no-feedback', ['&.phx-no-feedback', '.phx-no-feedback &'])),
-    plugin(({ addVariant }) => addVariant('phx-click-loading', ['&.phx-click-loading', '.phx-click-loading &'])),
-    plugin(({ addVariant }) => addVariant('phx-submit-loading', ['&.phx-submit-loading', '.phx-submit-loading &'])),
-    plugin(({ addVariant }) => addVariant('phx-change-loading', ['&.phx-change-loading', '.phx-change-loading &']))
+    require('@tailwindcss/typography'),
+    require('daisyui'),
+    // Allows prefixing tailwind classes with LiveView classes to add rules
+    // only when LiveView classes are applied, for example:
+    //
+    //     <div class="phx-click-loading:animate-ping">
+    //
+    plugin(({ addVariant }) => addVariant("phx-no-feedback", [".phx-no-feedback&", ".phx-no-feedback &"])),
+    plugin(({ addVariant }) => addVariant("phx-click-loading", [".phx-click-loading&", ".phx-click-loading &"])),
+    plugin(({ addVariant }) => addVariant("phx-submit-loading", [".phx-submit-loading&", ".phx-submit-loading &"])),
+    plugin(({ addVariant }) => addVariant("phx-change-loading", [".phx-change-loading&", ".phx-change-loading &"])),
+
+    // Embeds Hero Icons (https://heroicons.com) into your app.css bundle
+    // See your `CoreComponents.icon/1` for more information.
+    //
+    plugin(function ({ matchComponents, theme }) {
+      let iconsDir = path.join(__dirname, "../priv/hero_icons/optimized")
+      let values = {}
+      let icons = [
+        ["", "/24/outline"],
+        ["-solid", "/24/solid"],
+        ["-mini", "/20/solid"]
+      ]
+      icons.forEach(([suffix, dir]) => {
+        fs.readdirSync(path.join(iconsDir, dir)).map(file => {
+          let name = path.basename(file, ".svg") + suffix
+          values[name] = { name, fullPath: path.join(iconsDir, dir, file) }
+        })
+      })
+      matchComponents({
+        "hero": ({ name, fullPath }) => {
+          let content = fs.readFileSync(fullPath).toString().replace(/\r?\n|\r/g, "")
+          return {
+            [`--hero-${name}`]: `url('data:image/svg+xml;utf8,${content}')`,
+            "-webkit-mask": `var(--hero-${name})`,
+            "mask": `var(--hero-${name})`,
+            "background-color": "currentColor",
+            "vertical-align": "middle",
+            "display": "inline-block",
+            "width": theme("spacing.5"),
+            "height": theme("spacing.5")
+          }
+        }
+      }, { values })
+    })
   ]
 }
