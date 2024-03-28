28d96a3c851cf0881228e6b9d9c127a467d0b41f
Roberto Estrada <restrada@paridin.com>
2024-03-27 22:52:22 -0600

    update phoenix html

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule DaisyUi.MixProject do
   defp deps do
     [
       {:phoenix, "~> 1.7"},
-      {:phoenix_html, "~> 3.3"},
+      {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.18"},
       {:floki, ">= 0.30.0", only: :test},
